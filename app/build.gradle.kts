388f3e7953edbe9beadee0e0bd52afcd08c6fa3a
Javokhir Savriev <javokhir@Javokhirs-MacBook-Pro.local>
2022-07-28 14:17:15 +0500

    Migrate app build configuration from Groovy to KTS

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = ProjectConfig.compileSdk
+
+    defaultConfig {
+        applicationId = ProjectConfig.appId
+        minSdk = ProjectConfig.minSdk
+        targetSdk = ProjectConfig.targetSdk
+        versionCode = ProjectConfig.versionCode
+        versionName = ProjectConfig.versionName
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Compose.composeCompilerVersion
+    }
+}
+
+dependencies {
+    implementation(AndroidX.coreKtx)
+    implementation(AndroidX.appCompat)
+
+    implementation(Compose.runtime)
+    implementation(Compose.compiler)
+    implementation(Compose.ui)
+    implementation(Compose.uiToolingPreview)
+    implementation(Compose.material)
+    implementation(Compose.navigation)
+    implementation(Compose.hiltNavigationCompose)
+    implementation(Compose.viewModelCompose)
+    implementation(Compose.activityCompose)
+
+    implementation(Google.material)
+
+    implementation(DaggerHilt.hiltAndroid)
+    kapt(DaggerHilt.hiltCompiler)
+
+    kapt(Room.roomCompiler)
+    implementation(Room.roomKtx)
+    implementation(Room.roomRuntime)
+
+    implementation(Coil.coilCompose)
+}
---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,11 @@
+object Modules {
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":onboarding:domain"
+    const val onboardingPresentation = ":onboarding:presentation"
+
+    const val trackerData = ":tracker:data"
+    const val trackerDomain = ":tracker:domain"
+    const val trackerPresentation = ":tracker:presentation"
+}
